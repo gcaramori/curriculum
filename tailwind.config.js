2de3504563b927666a609805c5a32ec114d6e1b2
Guilherme <gcaramori16@gmail.com>
2022-05-18 20:04:16 -0300

    finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ["./index.html"],
   theme: {
     extend: {},
+    fontFamily: {
+      main: ['Lato', 'sans-serif']
+    },
+    colors: {
+      main: {
+        golden: '#eccc68' 
+      },
+      black: colors.black,
+      white: colors.white,
+      gray: colors.coolGray,
+      red: colors.red,
+      yellow: colors.amber,
+      green: colors.emerald,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      purple: colors.violet,
+      pink: colors.pink
+    }
   },
   plugins: [],
 }
